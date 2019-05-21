91df458ce929667c40aa8f9a61b29017cdefe82c
monicalopezgris <monicalopezgris@gmail.com>
2019-05-21 18:42:13 +0200

    update

--- routes/doc.js
+++ routes/doc.js
@@ -27,21 +27,65 @@ router.get('/doc/:id', async (req, res, next) => {
 
 router.post('/doc', async (req, res, next) => {
   const { status, items } = req.body;
-  const clientId = req.body.clientId ? req.body.clientId : undefined;
+  // const clientId = req.body.clientId ? req.body.clientId : undefined;
 
   try {
-    if (clientId) {
-      const doc = await Doc.create({
-        status,
-        data: {
-          clientId,
+    // if (clientId) {
+    //   const doc = await Doc.create({
+    //     status,
+    //     data: {
+    //       clientId,
+    //     },
+    //     items: items
+    //   });
+    //   res.status(200).json(doc);
+    // } else {
+    const { name, ref, cif, street, streetNum, postalCode, country } = req.body;
+    const doc = await Doc.create({
+      ref,
+      status,
+      data: {
+        client: {
+          name,
+          cif,
+          address: {
+            street,
+            streetNum,
+            postalCode,
+            country,
+          }
         },
         items: items
-      });
-      res.status(200).json(doc);
-    } else {
-      const { name, cif, street, streetNum, postalCode, country } = req.body;
-      const doc = await Doc.create({
+      }
+    });
+    res.status(200).json(doc);
+    // }
+
+  } catch (error) {
+    next(error);
+  }
+},
+);
+
+router.put('/doc/:id', async (req, res, next) => {
+  const { id } = req.params;
+  // const clientId = req.body.clientId ? req.body.clientId : false;
+
+  try {
+    const actualDoc = await Doc.findById(id);
+    if (actualDoc.status === "draft") {
+      // if (clientId) {
+      //   const doc = await Doc.findByIdAndUpdate(id, {
+      //     status,
+      //     clientId,
+      //     data: {
+      //       items: items
+      //     }
+      //   });
+      //   res.status(200).json(doc)
+      // } else {
+      const { name, status, cif, street, streetNum, postalCode, country, items } = req.body;
+      const doc = await Doc.findByIdAndUpdate(id, {
         status,
         data: {
           client: {
@@ -59,53 +103,10 @@ router.post('/doc', async (req, res, next) => {
       });
       res.status(200).json(doc);
     }
-
-  } catch (error) {
-    next(error);
-  }
-},
-);
-
-router.put('/doc/:id', async (req, res, next) => {
-  const { id } = req.params;
-  const clientId = req.body.clientId ? req.body.clientId : false;
-
-  try {
-    const actualDoc = await Doc.findById(id);
-    if (actualDoc.status === "draft") {
-      if (clientId) {
-        const doc = await Doc.findByIdAndUpdate(id, {
-          status,
-          clientId,
-          data: {
-            items: items
-          }
-        });
-        res.status(200).json(doc)
-      } else {
-        const { name, status, cif, street, streetNum, postalCode, country, items } = req.body;
-        const doc = await Doc.findByIdAndUpdate(id, {
-          status,
-          data: {
-            client: {
-              name,
-              cif,
-              address: {
-                street,
-                streetNum,
-                postalCode,
-                country,
-              }
-            },
-            items: items
-          }
-        });
-        res.status(200).json(doc);
-      }
-    }
-    else {
-      res.status(500).json(doc)
-    }
+    // }
+    // else {
+    //   res.status(500).json(doc)
+    // }
   } catch (error) {
     next(error)
   }
